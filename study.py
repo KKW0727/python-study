27e054c681af666de681235c0abd5b77e450ff72
kkw0727 <kkw3401@gmaiil.com>
2022-09-21 18:44:19 +0900

    add: 条件分岐 if...elif...else

--- study.py
+++ study.py
@@ -106,3 +106,29 @@ print(f"2019: {number * RATE:,.1f}")
 print(f"2020: {number * RATE * RATE:,.2f}")
 print(f"2021: {number * RATE * RATE * RATE:,.3f}")
 
+#条件分岐
+#if...elif...else
+#elif を使えば複数の条件で場合分けをすることができる
+#Python では、字下げがあるかないかで全く違う意味になるので、注意!
+#字下げに使う空白は何文字でもいいけど、 Pythonでは半角空白 4文字が使われることが多い
+
+#比較演算子
+# > >=
+# < <=
+# == !=
+
+# 論理演算子
+# and なおかつ
+# or もしくは
+# not ~ではない
+
+japaneseScore = int(input("japaneseScore?"))
+koreanScore = int(input("koreanScore?"))
+if japaneseScore <= 0 and koreanScore <= 0:
+    print("ERROR")
+elif japaneseScore >= 80 or koreanScore >= 80:
+    print("合格")
+else:
+    print("NG")
+    print("不合格")
+
