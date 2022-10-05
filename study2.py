104a5fc8e72f31dab8e221ff00f88b371b05d25e
KKW0727 <gig2010@naver.com>
2022-10-06 01:19:16 +0900

    add: キーワード引数、位置引数

--- study2.py
+++ study2.py
@@ -15,3 +15,20 @@ showName("masaki") #masaki suda
 
 #Pythonでは、関数でreturnを書かなかった場合、 Noneをreturn することになっている ex)return None
 #関数にreturnがなかったら関数を呼び出してもNoneが返ってくる
+
+
+
+#キーワード引数、位置引数
+
+def greet(name,by): #name. byは仮引数と呼ぶ
+    print(f"{by} said, hi {name}")
+#どちらがどちらだったか分かりづらくなる場合もある
+#その場合、実引数にキーワードを指定することができる
+
+greet(name="aimyon", by="gen") #aimyon, genは実引数と呼ぶ
+                               #by="gen" このような書き方をキーワード引数と呼ぶ
+greet(name="kenji", by="suda") 
+                               #この場合は順序で仮引数に渡されていくから位置が重要という意味で位置引数と呼ぶ
+                               
+#greet(name="kenji", "suda") エラー
+#キーワード引数と位置引数を一緒に書くことはできない
